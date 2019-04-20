e8646f48bc8359896e6cedeb98cb1a12e28a07b0
Chris Michalsen <chris.michalsen@gmail.com>
2019-04-20 12:36:17 -0700

    uncommented part 1

--- Lab2/main.c
+++ Lab2/main.c
@@ -6,15 +6,15 @@
 int main(void)
 {
 	// PART 1
-		//DDRB = (1 << DDB5);	//	OUTPUT
-		//
-		//while(1)
-		//{
-			//PORTB = (1 << DDB5);	//	HIGH PIN 13
-			//_delay_ms(500);
-			//PORTB = 0;	// ALL OFF
-			//_delay_ms(500);
-		//}
+		DDRB = (1 << DDB5);	//	OUTPUT
+		
+		while(1)
+		{
+			PORTB = (1 << DDB5);	//	HIGH PIN 13
+			_delay_ms(500);
+			PORTB = 0;	// ALL OFF
+			_delay_ms(500);
+		}
 	
 	// PART 2
 		//DDRB = 0b00110000;	//	SETS BITS 4,5 TO OUPUT
