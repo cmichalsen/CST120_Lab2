c83cb883ddf4d0f847dd9c1244ab5013a9b70446
Chris Michalsen <chris.michalsen@gmail.com>
2019-04-20 12:32:50 -0700

    initial commit

---
+++ Lab2/main.c
@@ -0,0 +1,45 @@
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	// PART 1
+		//DDRB = (1 << DDB5);	//	OUTPUT
+		//
+		//while(1)
+		//{
+			//PORTB = (1 << DDB5);	//	HIGH PIN 13
+			//_delay_ms(500);
+			//PORTB = 0;	// ALL OFF
+			//_delay_ms(500);
+		//}
+	
+	// PART 2
+		//DDRB = 0b00110000;	//	SETS BITS 4,5 TO OUPUT
+		//
+		//while(1)
+		//{
+			//PORTB = 0b00110000;	//	SETS BITS 4,5 TO HIGH
+			//_delay_ms(500);
+			//PORTB = 0b00000000;	// ALL OFF
+			//_delay_ms(500);
+		//}
+	
+	// PART 3
+		//DDRB = 0b00110000;	//	SETS BITS 4,5 TO OUPUT
+		//
+		//while(1)
+		//{
+			//PORTB = 0b00110000;	//	SETS BITS 4,5 TO HIGH
+			//_delay_ms(500);
+			//PORTB = 0b00000000;	// ALL OFF
+			//_delay_ms(500);
+			//PORTB = 0b00010000;	//	SET BIT 4 TO HIGH
+			//_delay_ms(500);
+			//PORTB = 0b00000000;	// ALL OFF
+			//_delay_ms(500);			
+		//}
+	
+}
